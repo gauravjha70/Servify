7d1a5aa547deb2b95e49def686043bdb278ead1c
gauravjha70 <38986606+gauravjha70@users.noreply.github.com>
2019-09-21 21:01:42 +0530

    WLAN details extraction

--- app/src/main/java/com/example/servify/WifiInfoActivity.java
+++ app/src/main/java/com/example/servify/WifiInfoActivity.java
@@ -1,14 +1,163 @@
 package com.example.servify;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.IntentFilter;
+import android.net.DhcpInfo;
+import android.net.wifi.WifiManager;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Debug;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class WifiInfoActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class WifiInfoActivity extends AppCompatActivity{
+
+
+    private Button btnScan;
+
+    ArrayList<String> ipList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wifi_info);
+        btnScan = findViewById(R.id.search);
+
+        WifiManager wifiManager;
+        wifiManager = (WifiManager)getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        DhcpInfo dhcpInfo = wifiManager.getDhcpInfo();
+        String defaultGateway = String.valueOf(dhcpInfo.gateway);
+        String subnetMask = String.valueOf(dhcpInfo.netmask);
+
+        ipList = new ArrayList();
+        btnScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new ScanIpTask().execute();
+            }
+        });
+
     }
+
+    private class ScanIpTask extends AsyncTask<Void, String, Void> {
+
+        /*
+        Scan IP 192.168.1.100~192.168.1.110
+        you should try different timeout for your network/devices
+         */
+        static final String subnet = "192.168.43.";
+        static final int lower = 195;
+        static final int upper = 200;
+        static final int timeout = 5000;
+
+        @Override
+        protected void onPreExecute() {
+            ipList.clear();
+            Toast.makeText(getApplicationContext(), "Scan IP...", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            for (int i = lower; i <= upper; i++) {
+                String host = subnet + i;
+
+                try {
+                    InetAddress inetAddress = InetAddress.getByName(host);
+                    if (inetAddress.isReachable(timeout)){
+                        publishProgress(inetAddress.toString());
+                    }
+
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            ipList.add(values[0]);
+            System.out.println("IpAddress : " + values[0]);
+            System.out.println("IpAddress : " + getMacFromArpCache(values[0]));
+            try{
+                InetAddress addr = InetAddress.getByName(values[0]);
+                String host = addr.getHostName();
+                System.out.println("Host : " + host);
+            }
+            catch (Exception e){}
+
+            Toast.makeText(getApplicationContext(), values[0], Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            Toast.makeText(getApplicationContext(), "Done", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public static String getMacFromArpCache(String ip) {
+        if (ip == null)
+            return null;
+        String[] ipList = ip.split("/");
+        ip = ipList[1];
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader("/proc/net/arp"));
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+                String[] splitted = line.split(" +");
+                if (splitted != null && splitted.length >= 4 && ip.equals(splitted[0])) {
+                    // Basic sanity check
+                    String mac = splitted[3];
+                    if (mac.matches("..:..:..:..:..:..")) {
+                        return mac;
+                    } else {
+                        System.out.println("Mac Matches Error");
+                        return null;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        } finally {
+            try {
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
 }
