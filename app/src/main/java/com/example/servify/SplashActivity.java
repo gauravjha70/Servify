939b91412daa5048bad5bcce6f2194358ec6b3f8
Shrey Sindher <ssindher11@gmail.com>
2019-09-21 18:09:38 +0530

    Added Login and Splash Screens

--- app/src/main/java/com/example/servify/SplashActivity.java
+++ app/src/main/java/com/example/servify/SplashActivity.java
@@ -1,6 +1,10 @@
 package com.example.servify;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +14,20 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        ImageView imageView = findViewById(R.id.iv_splash);
+        Animation splash = AnimationUtils.loadAnimation(this, R.anim.splash_animation);
+
+        imageView.startAnimation(splash);
+        int SPLASH_TIME_OUT = 500;
+        new android.os.Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                overridePendingTransition(R.anim.fadein, R.anim.fadeout);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
     }
 }
