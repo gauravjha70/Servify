076e6a18a0c38d27a6705777e3464faa1d38bf4a
gauravjha70 <38986606+gauravjha70@users.noreply.github.com>
2019-09-21 10:33:52 +0530

    Addition of AR and Wifi Info Module

--- app/src/main/java/com/example/servify/MainActivity.java
+++ app/src/main/java/com/example/servify/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.servify;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button camera, wifiInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        camera = findViewById(R.id.camera);
+        wifiInfo = findViewById(R.id.wifi_info);
+
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, CameraActivity.class));
+            }
+        });
+
+
+        wifiInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, WifiInfoActivity.class));
+            }
+        });
+
     }
 }
