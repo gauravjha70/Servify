243cc9c22cb8989877fa5a208742f734a7bf3fa6
gauravjha70 <38986606+gauravjha70@users.noreply.github.com>
2019-09-21 20:58:46 +0530

    WLAN details extraction

--- app/src/main/java/com/example/servify/SplashActivity.java
+++ app/src/main/java/com/example/servify/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.example.servify;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +11,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
     }
 }
