b9aea6d0ec767157dc3a800131f52784da3d6808
Shrey Sindher <ssindher11@gmail.com>
2019-09-22 00:55:11 +0530

    Implemented Login

--- app/src/main/java/com/example/servify/LoginActivity.java
+++ app/src/main/java/com/example/servify/LoginActivity.java
@@ -1,14 +1,118 @@
 package com.example.servify;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 9001;
+    private static final String TAG = "LoginActivity";
+    private ImageView googleIV, facebookIV;
+    private SharedPreferences sharedPreferences;
+
+    private FirebaseAuth mAuth;
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initViews();
+        initListeners();
+
+        sharedPreferences = getSharedPreferences("UserInfo", Context.MODE_PRIVATE);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(LoginActivity.this, gso);
+
+    }
+
+    private void initViews() {
+        googleIV = findViewById(R.id.iv_google);
+        facebookIV = findViewById(R.id.iv_facebook);
+    }
+
+    private void initListeners() {
+        googleIV.setOnClickListener(v -> {
+            Intent signinIntent = mGoogleSignInClient.getSignInIntent();
+            startActivityForResult(signinIntent, RC_SIGN_IN);
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
+
+//        showProgressDialog();
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Log.d(TAG, "signInWithCredential:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        updateUI(user);
+                    } else {
+                        Log.w(TAG, "signInWithCredential:failure", task.getException());
+                        updateUI(null);
+                    }
+
+//                    hideProgressDialog();
+                });
+    }
+
+    private void updateUI(FirebaseUser firebaseUser) {
+        if (firebaseUser != null) {
+            /*String name = firebaseUser.getDisplayName();
+            String uid = firebaseUser.getUid();*/
+            String email = firebaseUser.getEmail();
+
+            Toast.makeText(this, "Email: " + email, Toast.LENGTH_SHORT).show();
+
+        } else {
+            Toast.makeText(this, "Null USER", Toast.LENGTH_SHORT).show();
+        }
     }
 }
