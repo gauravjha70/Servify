2e296f20c27c8b47a2e2b3d67e6fe196132df68c
Shrey Sindher <ssindher11@gmail.com>
2019-09-22 16:14:55 +0530

    Added Image Slider

--- app/src/main/java/com/example/servify/MainActivity.java
+++ app/src/main/java/com/example/servify/MainActivity.java
@@ -7,10 +7,13 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.servify.ARModule.DetectorActivity;
+import com.smarteist.autoimageslider.IndicatorAnimations;
+import com.smarteist.autoimageslider.SliderView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button camera, wifiInfo;
+    private SliderView sliderView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +22,12 @@ public class MainActivity extends AppCompatActivity {
 
         camera = findViewById(R.id.camera);
         wifiInfo = findViewById(R.id.wifi_info);
+        sliderView = findViewById(R.id.sv_onboarding);
+        SliderAdapterOnboard adapter = new SliderAdapterOnboard(this);
+        sliderView.setSliderAdapter(adapter);
+        sliderView.setIndicatorAnimation(IndicatorAnimations.WORM);
 
         camera.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, DetectorActivity.class)));
-
 
         wifiInfo.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, WifiInfoActivity.class)));
 
